606a490d43bb31495dafd283aa3057786fa78f77
Davey Smit <davey.smit10@gmail.com>
2024-01-08 16:42:41 +0100

    Remove old code, set Build step to explicitly skip over restoring dependencies

--- Domain/DTO/DonationDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.DTO
-{
-    public class DonationDTO
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public double Amount { get; set; }
-    }
-}
--- PublishDonationMessageOntoQueue/Domain/Donation.cs
+++ PublishDonationMessageOntoQueue/Domain/Donation.cs
@@ -9,8 +9,8 @@ namespace PublishDonationMessageOntoQueue.Domain
         public int UserId { get; set; }
         public double Amount { get; set; }
         public int ArticleId { get; set; }
-        public string PartitionKey { get; set; }
-        public string RowKey { get; set; }
+        public string? PartitionKey { get; set; }
+        public string? RowKey { get; set; }
         public DateTimeOffset? Timestamp { get; set; }
         public ETag ETag { get; set; }
     }
