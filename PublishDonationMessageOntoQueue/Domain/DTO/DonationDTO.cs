9350b369bec1991af645b5cb98c8a62c5c9d388f
Davey Smit <davey.smit10@gmail.com>
2024-01-16 03:56:29 +0100

    Cleanup on isle 6

--- PublishDonationMessageOntoQueue/Domain/DTO/DonationDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PublishDonationMessageOntoQueue.Domain.DTO
-{
-    public class DonationDTO
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public double Amount { get; set; }
-    }
-}
